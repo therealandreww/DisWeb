ccb402d0688c37f7baae3f2a8af7de30911e855b
Heewoon Kim <heewoonkim2020@gmail.com>
2022-03-04 22:28:53 +0900

    Rate limits - step 1

---
+++ docs/static/js/ratelimit.js
@@ -0,0 +1,4 @@
+function rateLimitUser(blockApi, rateApi, cloudflareApi)    {
+    const cloudflareApiKey = "aDGSDJF13t6";
+    const cloudflareKey = "https://cloudflare.com/api/" + cloudflareApiKey;
+}
